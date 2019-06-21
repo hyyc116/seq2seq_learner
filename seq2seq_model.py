42929bd924e21997136c863c302885a4b42e5df5
hyyc116 <hyyc116@gmail.com>
2019-06-21 15:54:54 +0800

    add

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -16,18 +16,111 @@ import os
 import time
 
 
-class encoder(tf.keras.Model):
+def gru(units):
+    # If you have a GPU, we recommend using CuDNNGRU(provides a 3x speedup than GRU)
+    # the code automatically does that.
+    if tf.test.is_gpu_available():
+        return tf.keras.layers.CuDNNGRU(units, 
+                                        return_sequences=True, 
+                                        return_state=True, 
+                                        recurrent_initializer='glorot_uniform')
+    else:
+        return tf.keras.layers.GRU(units, 
+                                   return_sequences=True, 
+                                   return_state=True, 
+                                   recurrent_activation='sigmoid', 
+                                   recurrent_initializer='glorot_uniform')
 
-    def __init__(self):
+class Encoder(tf.keras.Model):
 
-        pass
+    def __init__(self,vocab_size,embedding_dim,enc_units,batch_sz):
+        super(Encoder.self).__init__()
 
-class decoder(tf.keras.Model):
+        self._vocab_size = vocab_size
+        self._embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self._enc_units = enc_units
+        self._batch_sz = batch_sz
 
-    pass
+        self._gru = gru(self._enc_units)
+
+    ##定义前向传播方法
+    def call(self,x,hidden):
+        ## 将输入转化为embedding
+        x = self.embedding(x)
+
+        ## 使用gru的RNN进行前向传播，得到每一步的输出以及state
+        output,state = self.gru(x,initial_state = hidden)
+        return output,state
+
+    def initialize_hidden_state(self):
+        ## 初始状态是 batch size x enc_units的大小
+        return tf.zeros((self._batch_sz,self._enc_units))
 
 
-class attention():
 
-    pass
+class BahdanauAttention(tf.keras.Model):
+
+    def __init__(self,units):
+        super(BahdanauAttention.self).__init__()
+
+        self._W1 = tf.keras.layers.Dense(units)
+
+        self._W2 = tf.keras.layers.Dense(units)
+
+        self._V = tf.keras.layers.Dense(1)
+
+    def call(self,query,values):
+
+        hidden_with_time_axis = tf.expand_dims(query,1)
+
+        score = self._V(tf.nn.tanh(self._W1(values)+self._W2(hidden_with_time_axis)))
+
+        attention_weights = tf.nn.softmax(score,axis=1)
+
+        context_vector = attention_weights*values
+
+        context_vector = tf.reduce_sum(context_vector,axis=1)
+
+        return context_vector,attention_weights
+
+
+
+class Decoder(tf.keras.Model):
+
+    def __init__(self,vocab_size,embedding_dim,dec_units,batch_sz):
+
+        super(Decoder.self).__init__()
+
+        self._batch_sz = batch_sz
+
+        self._dec_units = dec_units
+        ## encoder decoder的embedding是不共享的
+        self._embedding = tf.keras.layers.Embedding(vocab_size,embedding_dim)
+
+        self._gru = gru(self.dec_units)
+
+        self._fc = tf.keras.layers.Dense(vocab_size)
+
+        ## Attention
+        self._attention = BahdanauAttention(self._dec_units)
+
+    def call(self,x,hidden,enc_output):
+
+        ## encoder的输出是 batch size x length * hidden size
+        context_vector,attention_weights = self._attention(hidden,enc_output)
+
+        ## decoder自己的embedding
+        x = self._embedding(x)
+
+        ## 将context vector与就之前的output进行串联
+        x = tf.concat([tf.expand_dims(context_vector,1),x],axis=-1)
+
+        output,state = self.gru(x)
+
+        output = tf.reshape(output,(-1,output.shape[2]))
+
+        x = self._fc(output)
+
+        return x,state,attention_weights
+
 
--- seq2seq_trainer.py
+++ seq2seq_trainer.py
@@ -13,6 +13,7 @@ tf.enable_eager_execution()
 import numpy as np 
 import os
 import time
+from scipy import stats
 
 ## 数据的位置
 ## 数据的格式是 pos===========================title===========================content
@@ -29,38 +30,45 @@ def load_data():
 
         pos,title,content = splits
 
-        # poses.append(pos)
-        # titles.append(title)
-        # contents.append(content)
+        content = content.replace('-','')
 
-        data.append(pos,title,content)
+        if len(content) < 100:
+            continue
+
+        data.append([pos,title,content])
+
+    # print(data[:1])
 
     #对输入输出进行编号
-    input_wix =  WordIndex([content for pos,title,content in data])
-    target_wix = WordIndex([title for pos,title,content in data])
+    input_wix =  WordIndex(content for pos,title,content in data)
+    target_wix = WordIndex(title for pos,title,content in data)
 
     ##对输入输出进行转化
-    input_tensor = [[input_wix[w] for w in title for pos,title,content in data]]
-    target_tensor = [target_wix[w] for w in content for pos,title,content in data]
+    input_tensor = [[input_wix.word2ix[w] for w in content] for pos,title,content in data]
+    target_tensor = [[target_wix.word2ix[w] for w in title] for pos,title,content in data]
 
     ## 计算输入输出的最大长度
-    max_length_inp,max_length_tar = max_length(input_tensor),max_length(target_tensor)
-    print('max length of input data is %d, and  max length of target data is %d.'% (max_length_inp,max_length_tar))
+    max_length_inp,max_length_tar = mode_length(input_tensor),max_length(target_tensor)
+    print('mode length of input data is %d, and  max length of target data is %d.'% (max_length_inp,max_length_tar))
 
     ## 根据最大长度对输入输出进行padding
-    input_tensor = tf.keras.preprocessing.sequence.pad_sequences(input_tensor,maxlen=max_length_inp,padding='post')
-    target_tensor= tf.keraas.preprocessing.sequence.pad_sequences(target_tensor,maxlen=max_length_tar,padding='post')
+    input_tensor = tf.keras.preprocessing.sequence.pad_sequences(input_tensor,maxlen=max_length_inp,padding='post',truncating='post')
+    target_tensor= tf.keras.preprocessing.sequence.pad_sequences(target_tensor,maxlen=max_length_tar,padding='post',truncating='post')
+
+    return input_tensor,target_tensor,max_length_inp,max_length_tar,input_wix,target_wix
 
 
-    return input_tensor,output_tensor,max_length_inp,max_length_tar,input_wix,target_wix
+def mean_length(tensor):
+    return int(np.mean([len(t) for t in tensor]))
 
+def mode_length(tensor):
+    return int(stats.mode([len(t) for t in tensor])[0])
 
 def max_length(tensor):
-    return max(len(t) for t in tensor)
-
+    return int(np.max([len(t) for t in tensor]))
 
 ## 将所有的字进行index，转化为数字
-class WordIndex:
+class WordIndex():
 
     def __init__(self,words):
         self._words = words
@@ -76,11 +84,12 @@ class WordIndex:
 
         ## 每一个字和符号
         for word in self._words:
-            self.vocab.add(word)
+            for c in word:
+                self.vocab.add(c)
 
         self.vocab = sorted(self.vocab)
 
-        self.word2ix['<pad>'] == 0
+        self.word2ix['<pad>'] = 0
 
         for ix,word in enumerate(self.vocab):
             self.word2ix[word] = ix
